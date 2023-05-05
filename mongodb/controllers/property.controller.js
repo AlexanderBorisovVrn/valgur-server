41a58ae9bee748abe74765aeb239653751f08200
alexanderborisovvrn <alexanderborisovvrn@gmail.com>
2023-05-05 16:27:38 +0300

    feat:add create property

--- mongodb/controllers/property.controller.js
+++ mongodb/controllers/property.controller.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import UserModel from '../models/user.js';
 import PropertyModel from '../models/property.js'
 import { v2 as cloudinary } from 'cloudinary'
-import { cloudinaryApiKey, cloudinaryApiSecret, cloudinaryName } from 'enviroment.js'
+import { cloudinaryApiKey, cloudinaryApiSecret, cloudinaryName } from '../../enviroment.js'
 
 cloudinary.config({
     cloud_name: cloudinaryName,
@@ -42,12 +42,12 @@ const createProperty = async (req, res) => {
             propertyType,
             description,
             location,
-            photo: photoUrl,
-            creator: user_id
+            photo: photoUrl.url,
+            creator: user._id
         })
         user.allProperties.push(newProperty._id)
         await user.save({ session })
-        await session.commitTransaction
+        await session.commitTransaction()
         res.status(200).send("Property created successfully")
     } catch (error) {
         res.status(500).send(error)
